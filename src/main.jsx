0355197e6331a2f7e41a5904bff710d7fa11c23f
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-09-23 17:57:51 +0900

    軽微な修正

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router basename="/react_todo_list">
+      <App />
+    </Router>
   </StrictMode>,
 )
