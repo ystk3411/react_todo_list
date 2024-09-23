ff58574a1ef9d20298ebd958e63a086192901612
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-09-23 17:36:01 +0900

    軽微な修正

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react_todo_list/",
-  plugins: [react()]
+  plugins: [react()],
+  base: "/react_todo_list/"
 })
